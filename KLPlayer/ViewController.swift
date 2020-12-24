44fb3959a954871e55bbdc88d5c5ce2c7718f9be
kaili <fmslikai@gmail.com>
2020-12-24 10:16:38 +0800

    Initial Commit

---
+++ KLPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KLPlayer
+//
+//  Created by kaili on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
